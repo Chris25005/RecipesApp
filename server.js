d12cd9fe7a9b199bcd560d308f6a2bf1cc3d7f49
Chris25005 <chriswakar@gamil.com>
2025-09-14 01:26:11 +0530

    database connectivity --done

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 // import express module
 const express = require('express');
+const mongoose = require ('mongoose');
+require('dotenv').config();
 
 // create an express application
 const app = express();
 
 app.get('/', (req, res) => {
     res.json ({ message: 'hello express!'});
-})
+});
 
 // listen for incoming requests ( start teh server)
 
 app.listen(3001, 'localhost', () => {
     console.log('server is running on http://localhost:3001');
-})
+});
+
+// connect to MongoDB
+mongoose
+    .connect (process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('could not connect to MongoDB', err)); 
